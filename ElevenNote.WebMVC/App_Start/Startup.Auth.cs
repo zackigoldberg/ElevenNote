ea16eca46476d075af7ffe6b3dcdd7aee35291df
zackigoldberg <zackigoldberg@gmail.com>
2018-11-21 14:13:35 -0500

    Controller built

--- ElevenNote.WebMVC/App_Start/Startup.Auth.cs
+++ ElevenNote.WebMVC/App_Start/Startup.Auth.cs
@@ -12,7 +12,7 @@ namespace ElevenNote.WebMVC
 {
     public partial class Startup
     {
-        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+        // For more information on configuring austhentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
